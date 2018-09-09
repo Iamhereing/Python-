133d9fc37f72196d7b159e2a7b9d1f1c368f9052
Iamhereing <759188632@qq.com>
2018-09-10 06:44:33 +0800

    武装飞船

--- 第12章/alien_invasion/settings.py
+++ 第12章/alien_invasion/settings.py
@@ -1,9 +1,9 @@
 class Settings():
     """储存《外星人入侵》所有设置的类"""
 
-    def __index__(self):
+    def __init__(self):
         """初始化游戏的设置"""
         #屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color =(255,0,0)
+        self.bg_color =(230,230,230)
