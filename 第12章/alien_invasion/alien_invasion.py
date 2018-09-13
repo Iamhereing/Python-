67c2d7c8ee4ed7a30aec1e8b1f5ab3b0c27d7b0b
Iamhereing <759188632@qq.com>
2018-09-13 09:03:46 +0800

    外星人入侵

--- 第12章/alien_invasion/alien_invasion.py
+++ 第12章/alien_invasion/alien_invasion.py
@@ -8,7 +8,11 @@ from settings import Settings
 
 from ship import Ship
 
+from alien import Alien
+
 import game_functions as  gf
+
+from game_stats import GameStats
 
 def run_game():
     # 初始化pygame，设置和屏幕对象
@@ -16,6 +20,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一个用于存储游戏统计信息的实例
+    stats =GameStats(ai_settings)
 
     # 设置背景色
     # bg_color = (230,230,230)
@@ -26,13 +33,23 @@ def run_game():
     # 创建一个用于存储子弹的编组
     bullets = Group()
 
+    # 创建一个外星人编组
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
     # 开始游戏主循环
     while True:
         # 监视键盘和鼠标事件
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
