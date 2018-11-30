c2baff11f8f66b5a8a36d93793ed0572b085379b
iamhereing <1161564825@qq.com>
2018-11-30 10:13:07 +0800

    下载数据

--- 项目/数据可视化/btc_close_2017.py
+++ 项目/数据可视化/btc_close_2017.py
@@ -31,14 +31,44 @@ line_chart.add('log收盘价', close_log) # 增加属性标签
 line_chart.render_to_file('收盘价对数变换折线图（￥）.svg')
 
 def draw_line(x_date, y_date, title, y_legend):
-    xy_map = []
+    xy_map = [] # 将X轴与Y轴的数据合并
     for x, y in groupby(sorted(zip(x_date, y_date)), key=lambda _: _[0]):
-        y_list = [v for _, v in y]
-        xy_map.append([x, sum(y_list) / len(y_list)])
-    x_unique, y_mean = [*zip(*xy_map)]
+        y_list = [v for _, v in y] # 对X和Y轴日期进行排序，再用函数groupby分组
+        xy_map.append([x, sum(y_list) / len(y_list)]) # 求出每组的均值
+    x_unique, y_mean = [*zip(*xy_map)] # 将x轴和y轴的数据分离
     line_chart = pygal.Line()
     line_chart.title = title
     line_chart.x_labels = x_unique
     line_chart.add(y_legend, y_mean)
     line_chart.render_to_file(title+'.svg')
     return line_chart
+
+idx_month = dates.index('2017-12-01')
+line_chart_month = draw_line(months[:idx_month], close[:idx_month],
+                                 '收盘价月日均值（￥）', '月日均值')
+line_chart_month
+
+idx_week = dates.index('2017-12-11')
+line_chart_week = draw_line(weeks[1:idx_week], close[1:idx_week],
+                            '收盘价周日均值（￥）', '周日均值')
+line_chart_week
+
+idx_week = dates.index('2017-12-11')
+wd = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+weekdays_int = [wd.index(w) + 1 for w in weekdays[1:idx_week]]
+line_chart_weekday = draw_line(weekdays_int, close[1:idx_week],'收盘价星期均值（￥）',
+                               '星期均值')
+line_chart_weekday.x_labels = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+line_chart_weekday.render_to_file('收盘价星期均值（￥）.svg')
+
+with open('收盘价Dashboard.html', 'w', encoding='utf8') as html_file:
+    html_file.write('<html><head><title>收盘价Dashboard</title><meta '
+                    'charset="utf-8"</head><body>\n')
+    for svg in [
+        '收盘价折线图（￥）.svg', '收盘价对数变换折线图（￥）.svg',
+        '收盘价月日均值（￥）.svg', '收盘价周日均值（￥）.svg',
+        '收盘价星期均值（￥）.svg'
+     ]:
+        html_file.write('   <object type="image/svg+xml" data="{0}" '
+                        'height=500></object>\n'.format(svg))
+    html_file.write('</body></html>')
